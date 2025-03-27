9d04009ff61be3d80a5212180c03de4763999ce3
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-03-27 11:57:18 +0530

    Fixed TypeScript errors and implemented dynamic Navbar with categories and store name

--- ecommerce-store/app/[storeId]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function StorePage({ params }: { params: { storeId: string } }) {
-     return (
-          <div>
-               <main>{/* Page content */}</main>
-          </div>
-     );
-}
