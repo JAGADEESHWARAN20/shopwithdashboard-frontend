c5daea4babe62d8f1ac5267311ec8851995be2a6
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-04-08 17:56:05 +0530

    new updates in api

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl="/sign-up">
+    <ClerkProvider afterSignOutUrl="/">
     <html lang="en">
       <head>
         <Script
