713ebf0a6b6a3f3327e1fe6f8b64e7b7b8b5f566
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-04-08 14:22:19 +0530

    new updates in api

--- ecommerce-store/components/navbar-actions.tsx
+++ ecommerce-store/components/navbar-actions.tsx
@@ -42,7 +42,7 @@ const NavBarActions = () => {
                     </DrawerTrigger>
                     <DrawerContent className="p-4">
                          <div className="flex flex-col space-y-2">
-                              <SignInButton />
+                              <SignInButton forceRedirectUrl={'/(auth)/sign-in'} />
                               <SignUpButton />
                          </div>
                     </DrawerContent>
