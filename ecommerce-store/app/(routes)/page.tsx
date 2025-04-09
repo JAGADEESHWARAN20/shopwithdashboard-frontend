a86133180d2cdc89be132df9a0449379bde61c88
JAGADEESHWARAN <66456490+JAGADEESHWARAN20@users.noreply.github.com>
2025-04-09 22:06:27 +0530

    Update page.tsx

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -3,27 +3,27 @@ import Billboard from "@/components/billboard";
 import getBillboards from '@/actions/get-billboards';
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
-import UserDataSender from "@/utils/UserDataSender"; // Adjust the import path
+import SyncUser from "@/utils/UserDataSender"; // Adjust the import path accordingly
 
 export const revalidate = 0;
 
 const HomePage = async () => {
-     const products = await getProducts({ isFeatured: true });
-     const billboard = await getBillboards("e697928c-5915-4361-86c3-9cc0765b6b58");
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboards("e697928c-5915-4361-86c3-9cc0765b6b58");
 
-     return (
-          <>
-               <Container>
-               <UserDataSender /> 
-                    <div className="space-y-10 pb-10">
-                         <Billboard data={billboard} />
-                         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                              <ProductList title={"Featured Products"} items={products} />
-                         </div>
-                    </div>
-               </Container>
-          </>
-     );
+  return (
+    <>
+      <Container>
+        <SyncUser />
+        <div className="space-y-10 pb-10">
+          <Billboard data={billboard} />
+          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+            <ProductList title={"Featured Products"} items={products} />
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default HomePage;
