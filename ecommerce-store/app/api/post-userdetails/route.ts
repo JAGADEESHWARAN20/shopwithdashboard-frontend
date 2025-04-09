f7766caec045e05e06e2e4361927e6beff0a1093
JAGADEESHWARAN <66456490+JAGADEESHWARAN20@users.noreply.github.com>
2025-04-09 12:27:47 +0530

    Update route.ts

--- ecommerce-store/app/api/post-userdetails/route.ts
+++ ecommerce-store/app/api/post-userdetails/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { currentUser } from "@clerk/nextjs/server";
 
-export async function POST(req: NextRequest) {
+export async function POST() {
     const user = await currentUser();
 
     if (user?.id && user?.firstName && user?.emailAddresses?.[0]?.emailAddress) {
