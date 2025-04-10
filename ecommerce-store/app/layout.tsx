b6034936055bfc93e3d4a5ba476582745049fa6f
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-04-10 14:39:14 +0530

    new updates in api

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -1,6 +1,6 @@
+"use client"
 
 import "./globals.css";
-import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
@@ -13,10 +13,6 @@ import { UserProvider } from "@/contexts/user-context";
 
 const font = Urbanist({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: "Store",
-  description: "Store",
-};
 
 export default function RootLayout({
   children,
